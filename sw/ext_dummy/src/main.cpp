afa7a75569cac56ea2a24641ffa698f4ecb4b29e
RijoRoriz <ricardojrroriz@gmail.com>
2024-03-27 21:35:03 +0000

    fix the includes with the new hpp files

--- sw/ext_dummy/src/main.cpp
+++ sw/ext_dummy/src/main.cpp
@@ -14,10 +14,13 @@
  * limitations under the License.
  */
 
-#include "rclcpp/rclcpp.hpp"
-#include "alfa_node/alfa_node.hpp"
-#include <vector>
 #include <cmath>
+#include <vector>
+
+#include "alfa_defines.hpp"
+#include "alfa_node.hpp"
+#include "alfa_structs.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 #define NODE_NAME "ext_dummy"
 #define DEFAULT_TOPIC "/velodyne_points"
@@ -30,85 +33,77 @@
  *
  * @param node Pointer to the AlfaNode object handling the incoming point cloud.
  */
-void handler (AlfaNode * node)
-{
-    #ifdef EXT_HARDWARE
-        node->store_pointcloud(LOAD_STORE_CARTESIAN);
-    #else
-        AlfaPoint point;
-        while(node->get_point_input_pointcloud(point))
-        {       
-            node->push_point_output_pointcloud(point);
-        }
-    #endif
+void handler(AlfaNode *node) {
+#ifdef EXT_HARDWARE
+  node->store_pointcloud(LOAD_STORE_CARTESIAN);
+#else
+  AlfaPoint point;
+  while (node->get_point_input_pointcloud(point)) {
+    node->push_point_output_pointcloud(point);
+  }
+#endif
 }
 
-
 /**
- * @brief Post-processing function that publishes output point cloud and metrics.
+ * @brief Post-processing function that publishes output point cloud and
+ * metrics.
  *
  * @param node Pointer to the AlfaNode object handling the incoming point cloud.
  */
-void post_processing (AlfaNode * node)
-{
-    #ifdef EXT_HARDWARE
-        node->load_pointcloud(LOAD_STORE_CARTESIAN);
-    #endif
+void post_processing(AlfaNode *node) {
+#ifdef EXT_HARDWARE
+  node->load_pointcloud(LOAD_STORE_CARTESIAN);
+#endif
 
-    node->publish_pointcloud();
+  node->publish_pointcloud();
 }
 
 /**
  * @brief Entry point of the program.
- * 
+ *
  * @param argc The number of arguments.
  * @param argv The array of arguments.
  * @return int The exit code of the program.
  */
-int main(int argc, char **argv)
-{
-    // Initialize ROS 2
-    rclcpp::init(argc, argv);
+int main(int argc, char **argv) {
+  // Initialize ROS 2
+  rclcpp::init(argc, argv);
 
-    // Get subscriber topic from command line arguments
-    std::string subscriber_topic = DEFAULT_TOPIC;
-    if (argc > 1) {
-        subscriber_topic = argv[1];
-    }
+  // Get subscriber topic from command line arguments
+  std::string subscriber_topic = DEFAULT_TOPIC;
+  if (argc > 1) {
+    subscriber_topic = argv[1];
+  }
 
-    #ifdef EXT_HARDWARE
-        AlfaHardwareSupport hardware_support{false, true};
-    #else
-        AlfaHardwareSupport hardware_support{false, false};
-    #endif
+#ifdef EXT_HARDWARE
+  AlfaHardwareSupport hardware_support{false, true};
+#else
+  AlfaHardwareSupport hardware_support{false, false};
+#endif
 
+  AlfaConfiguration conf;
+  AlfaExtensionParameter parameters[10];
 
-    AlfaConfiguration conf;
-    AlfaExtensionParameter parameters[10];
+  conf.subscriber_topic = subscriber_topic;
+  conf.node_name = NODE_NAME;
+  conf.pointcloud_id = POINTCLOUD_ID;
+  conf.extension_id = NODE_ID;
+  conf.hardware_support = hardware_support;
+  conf.latency = 0;
+  conf.number_of_debug_points = 1;
+  conf.metrics_publishing_type = ALL_METRICS;
+  conf.custom_field_conversion_type = CUSTOM_FIELD_INTENSITY;
 
-    conf.subscriber_topic = subscriber_topic;
-    conf.node_name = NODE_NAME;
-    conf.pointcloud_id = POINTCLOUD_ID;
-    conf.extension_id = NODE_ID;
-    conf.hardware_support = hardware_support;
-    conf.latency = 0;
-    conf.number_of_debug_points = 1;
-    conf.metrics_publishing_type = ALL_METRICS;
-    conf.custom_field_conversion_type = CUSTOM_FIELD_INTENSITY;
+  parameters[0].parameter_value = 5;
+  parameters[0].parameter_name = "min_distance";
+  parameters[1].parameter_value = 20;
+  parameters[1].parameter_name = "max_distance";
 
+  // Create an instance of AlfaNode and spin it
+  rclcpp::spin(
+      std::make_shared<AlfaNode>(conf, parameters, &handler, &post_processing));
 
-    parameters[0].parameter_value = 5;
-    parameters[0].parameter_name = "min_distance";
-    parameters[1].parameter_value = 20;
-    parameters[1].parameter_name = "max_distance";
-
-    // Create an instance of AlfaNode and spin it
-    rclcpp::spin(std::make_shared<AlfaNode>(conf, 
-                                    parameters,
-                                    &handler, 
-                                    &post_processing));
-
-    // Shutdown ROS 2
-    rclcpp::shutdown();
-    return 0;
+  // Shutdown ROS 2
+  rclcpp::shutdown();
+  return 0;
 }
